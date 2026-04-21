2da12b32743fc64b442508349e27ee06b7a2c304
mp1924 <mp1924@srmist.edu.in>
2026-04-21 09:48:47 +0530

    Update TicTacToe.java

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,4 +1,3 @@
- UC2-Toss-to-decide-First-player-and-symbol
 import java.util.Random;
 
 public class TicTacToe {
@@ -31,79 +30,5 @@ public class TicTacToe {
 
     public static void main(String[] args) {
         decideFirstPlayer();
-
-import java.util.Scanner;
-
-public class TicTacToe {
-
- UC4-Convert-Slot-Number-to-Board-index
-   
- UC3-Accept-User-Slot-Input
-    static Scanner scanner = new Scanner(System.in);
-
-    // UC3: Get user slot input
-    public static int getUserInput() {
-        int slot;
-
-        while (true) {
-            System.out.print("Enter a slot (1-9): ");
-
-            if (scanner.hasNextInt()) {
-                slot = scanner.nextInt();
-
-                if (slot >= 1 && slot <= 9) {
-                    return slot; // valid input
-                } else {
-                    System.out.println("Invalid input! Please enter a number between 1 and 9.");
-                }
-            } else {
-                System.out.println("Invalid input! Please enter a number.");
-                scanner.next(); // clear invalid input
-            }
- main
-    static char[][] board = {
-        {'-', '-', '-'},
-        {'-', '-', '-'},
-        {'-', '-', '-'}
-    };
-
-    // UC5: Validate move
-    public static boolean isValidMove(int row, int col) {
-
-        // Check bounds
-        if (row < 0 || row > 2 || col < 0 || col > 2) {
-            return false;
-        }
-
-        // Check if cell is empty
-        if (board[row][col] != '-') {
-            return false;
- UC4-Convert-Slot-Number-to-Board-in  
- main
-        }
-
-        return true;
-    }
-
-    public static void main(String[] args) {
- UC4-Convert-Slot-Number-to-Board-index
-
-        int row = 1;
-
-UC3-Accept-User-Slot-Input
-        int userMove = getUserInput();
-        System.out.println("You selected slot: " + userMove);
-
-      int row = 1;
- main
-        int col = 1;
-
-        if (isValidMove(row, col)) {
-            System.out.println("Valid move");
-        } else {
-            System.out.println("Invalid move");
-        }
- UC4-Convert-Slot-Number-to-Board-index
-    main
     }
 }
